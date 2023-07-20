8dcc7437d55178f7dc273f42263556dc471e52c3
MansiRavindraMohan <manasimohan29@gmail.com>
2023-07-20 10:53:32 +0530

    [Mansi]: Enter valid first name and check using regex

---
+++ Day9_Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9_Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            FirstName FName = new FirstName();
+            Console.WriteLine("Please enter the First name:");
+
+            string firstName = Console.ReadLine();
+            bool val = FName.validateFirstName(firstName);
+
+            if (val)
+            {
+                Console.WriteLine("First name matched");
+            }
+            else
+            {
+                Console.WriteLine("First name not matched");
+            }
+            Console.ReadLine();
+        }
+    }
+}
